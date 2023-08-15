717ba0752b39fc2216c287afeb7f68beaa33f2c7
Soha <sohaali710@gmail.com>
2023-08-15 19:10:17 +0300

    Commit

--- utils/apiError.js
+++ utils/apiError.js
@@ -1,9 +1,11 @@
 /**
  * @desc This class is responsible about operational errors (error I can predict)
  */
+
 class ApiError extends Error {
     constructor(statusCode, message) {
         super(message)
+
         this.statusCode = statusCode
         this.status = `${statusCode}`.startsWith(4) ? 'fail' : 'error' // status~400 -> fail , status~500 -> error
         this.isOperational = true // == is it predictable for me
