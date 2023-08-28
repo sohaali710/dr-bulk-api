05e57edbddbaf8f989fd5569364fcf9a44584332
Soha <sohaali710@gmail.com>
2023-08-28 21:28:13 +0300

    Edit to support En & Ar languages

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -16,28 +16,8 @@ const categorySchema = new mongoose.Schema({
             unique: [true, 'Ar Category must be unique'],
             minlength: [3, 'Too short ar category name'],
             trim: true
-        },
-        // type: String,
-        // require: [true, "Category name is required"],
-        // unique: [true, 'Category must be unique'],
-        // minlength: [3, 'Too short category name'],
-        // trim: true
+        }
     },
-    // nameEn: {
-    //     type: String,
-    //     require: [true, "En category name is required"],
-    //     unique: [true, 'En category must be unique'],
-    //     minlength: [3, 'Too short en category name'],
-    //     trim: true
-
-    // },
-    // nameAr: {
-    //     type: String,
-    //     require: [true, "Ar Category name is required"],
-    //     unique: [true, 'Ar Category must be unique'],
-    //     minlength: [3, 'Too short ar category name'],
-    //     trim: true
-    // },
     slug: {
         type: String,
         lowercase: true
