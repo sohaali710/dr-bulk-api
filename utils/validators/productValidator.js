d090673be0a9aaf354c199980a3e35d458aab87a
Soha <sohaali710@gmail.com>
2023-08-29 01:33:10 +0300

    Update product validation

--- utils/validators/productValidator.js
+++ utils/validators/productValidator.js
@@ -45,7 +45,8 @@ exports.createProductValidator = [
         .isNumeric().withMessage('Price should be a number')
         .isLength({ min: 1 }).withMessage('Price should be positive number'),
     check('points')
-        .optional(),
+        .optional()
+        .isNumeric().withMessage('Points should be a number'),
     // check('imageCover')
     //     .notEmpty().withMessage('Product cover image is required'),
     check('images')
@@ -61,13 +62,46 @@ exports.getProductValidator = [
 
 exports.updateProductValidator = [
     check('id').isMongoId().withMessage('Invalid product id format'),
+    check('title')
+        .isObject().withMessage('Product title must be an object'),
     check('title.en')
+        .notEmpty().withMessage('En Product title is required')
+        .isLength({ min: 3 }).withMessage('Too short En product title')
+        .isLength({ max: 32 }).withMessage('Too long En product title')
         .custom((val, { req }) => {
-            if (val) {
-                req.body.slug = slugify(val)
-            }
+            req.body.slug = slugify(val)
             return true
         }),
+    check('title.ar')
+        .notEmpty().withMessage('Ar Product title is required')
+        .isLength({ min: 3 }).withMessage('Too short Ar product title')
+        .isLength({ max: 32 }).withMessage('Too long Ar product title'),
+    check('category')
+        .notEmpty().withMessage('Product must be belong to category')
+        .isMongoId().withMessage('Invalid id format')
+        .custom(categoryId =>
+            Category.findById(categoryId).then(category => {
+                if (!category) {
+                    return Promise.reject(new Error(`No category for this id ${categoryId}`))
+                }
+            })
+        ),
+    check('description')
+        .isObject().withMessage('Product description must be an object'),
+    check('description.en')
+        .notEmpty().withMessage('En description is required')
+        .isLength({ min: 10 }).withMessage('Too short En description'),
+    check('description.ar')
+        .notEmpty().withMessage('Ar description is required')
+        .isLength({ min: 10 }).withMessage('Too short Ar description'),
+    check('price')
+        .notEmpty().withMessage('Product price is required')
+        .isNumeric().withMessage('Price should be a number')
+        .isLength({ min: 1 }).withMessage('Price should be positive number'),
+    check('points')
+        .optional()
+        .isNumeric().withMessage('Points should be a number'),
+
     validatorMiddleware
 ]
 
