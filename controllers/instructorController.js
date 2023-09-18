680150ca56358e16eb1bd2d54edeea85dad1c12a
Soha <sohaali710@gmail.com>
2023-09-18 22:24:36 +0300

    Edit in Add Instructor

--- controllers/instructorController.js
+++ controllers/instructorController.js
@@ -6,7 +6,7 @@ const Instructor = require('../models/instructorModel')
 exports.addInstructor = asyncHandler(async (req, res, next) => {
     const { name, bio, phoneNumber } = req.body
     let image = req.body.image
-    image = image ? req.files[0] : undefined
+    image = req.files ? req.files[0] : undefined
 
     const instructor = await Instructor.create({
         name,
