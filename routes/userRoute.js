3e17298517d927ce93822cdb6152874d8abfe699
Soha <sohaali710@gmail.com>
2023-10-05 15:15:48 +0300

    Handle get users to access editor or admin

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,13 +2,13 @@ const express = require('express')
 const router = new express.Router()
 const { signup, verifyEmailCode, login, getAllUsers, getProfile, updateProfile } = require('../controllers/userController')
 const { signupValidator, loginValidator, verifyEmailCodeValidator } = require('../utils/validators/userValidator')
-
+const EditorAdminAuth = require('../middlewares/editorAdminAuth')
 
 router.post('/signup', signupValidator, signup)
 router.get('/verify-email-code/:code/:id', verifyEmailCodeValidator, verifyEmailCode)
 router.post('/login', loginValidator, login)
 
-router.get('/all-users', getAllUsers)
+router.get('/all-users', EditorAdminAuth, getAllUsers)
 
 
 module.exports = router
