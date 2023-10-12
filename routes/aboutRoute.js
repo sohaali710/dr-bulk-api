a7300ff0ad421312e4e50f14a6efe5dd7309d8b0
Soha <sohaali710@gmail.com>
2023-10-12 12:32:52 +0300

    Handle about

--- routes/aboutRoute.js
+++ routes/aboutRoute.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 const { addAboutValidator } = require('../utils/validators/aboutValidator')
 const { addAbout, getAbout } = require('../controllers/aboutController')
-const AdminAuth = require('../middlewares/adminAuth')
+const EditorAdminAuth = require('../middlewares/editorAdminAuth')
 
 router
     .route('/')
-    .post(addAboutValidator, AdminAuth, addAbout)
+    .post(addAboutValidator, EditorAdminAuth, addAbout)
     .get(getAbout)
 
 module.exports = router
